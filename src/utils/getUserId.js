053d53775f28ec24381c9ad6520b3e836c187bfd
Mohit Agarwal <mohitagarwal@MacShik4ever.local>
2019-03-27 03:42:17 -0700

    configure env vars

--- src/utils/getUserId.js
+++ src/utils/getUserId.js
@@ -4,7 +4,7 @@ const getUserId = (request, authRequired = true) => {
     const header = request.request ? request.request.headers.authorization : request.connection.context.Authorization
     if (header) {
         const token = header.replace('Bearer ', '')
-        const decoded = jwt.verify(token, 'secret')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         return decoded.userId
     }
 
